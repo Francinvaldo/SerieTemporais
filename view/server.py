c5f6640bb591c46b17be72b897dbd63c77763827
Francinvaldo Silva <francinvaldo.wise@gmail.com>
2019-12-28 21:07:24 -0200

    criar um templete server

--- view/server.py
+++ view/server.py
@@ -1,7 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
+
+
 def index():
     return "Serie temporais"
 if __name__ == "__main__":
+
+
+
+
+
+
     app.run(host="0.0.0.0", debug=True)
